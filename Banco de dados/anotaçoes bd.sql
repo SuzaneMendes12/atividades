5d55fceb5699d2f3584a16b5c477d4bd9b23518e
SuzaneMendes12 <suzane.lima@sptech.school>
2023-11-07 12:42:09 -0300

    subindo

---
+++ Banco de dados/anotaçoes bd.sql
@@ -0,0 +1,9 @@
+-- funçoes matematicas
+-- count mostra tudo qtd total de registros
+-- group by mostra quantas coisas o mesmo item esta cadastrado  ex. aluno esta cadastrado em quantos cursos
+-- round arredonda , truncate limpa o arredondamento.
+--  max pega mairo nota e min menor nota
+-- funçoes de grupos      podem ser usadas todas juntas
+-- 
+-- sub query
+--  
--- anotaçoes bd.sql
+++
@@ -1,5 +0,0 @@
--- funoes matematicas
--- count mostra tudo qtd total de registros
--- group by mostra quantas coisas o mesmo item esta cadastrado  ex. aluno esta cadastrado em quantos cursos
---
--- 
